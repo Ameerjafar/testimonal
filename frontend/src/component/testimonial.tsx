0a6a653f54f4a9e70308dc4d03bd0bd7e3f8ca53
Ameer jafar <ameerjafar123@gmail.com>
2024-07-22 08:57:35 +0530

    added some more demo code

--- frontend/src/component/testimonial.tsx
+++ frontend/src/component/testimonial.tsx
@@ -4,6 +4,7 @@ export const testimonial = () => {
         <div>
             This is from the testimonial component.
             This is the main business logic over here.
+            this is the new thing.
         </div>
     )
 }
