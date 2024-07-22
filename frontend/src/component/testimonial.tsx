a8348cbd0910c6bcf25a5baccb396836b865a768
Ameer jafar <ameerjafar123@gmail.com>
2024-07-22 09:01:38 +0530

    remove some more line

--- frontend/src/component/testimonial.tsx
+++ frontend/src/component/testimonial.tsx
@@ -3,7 +3,6 @@ export const testimonial = () => {
     return(
         <div>
             This is from the testimonial component.
-            This is the main business logic over here.
         </div>
     )
 }
